0536280edf206beea55ad4105c38b87767bef1e2
Nicholas Szeto <szetonick@gmail.com>
2023-03-03 16:22:20 +0800

    New Project

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(menu) { section in
+                    Text(section.name)
+                    
+                    ForEach(section.items) { item in
+                        Text(item.name)
+                    }
+                }
+            }
+            .navigationTitle("Menu")
         }
-        .padding()
+        
     }
 }
 
