2e8d7830f185bd3f5050e90b27ef25bce174dc08
Nicholas Szeto <szetonick@gmail.com>
2023-03-03 14:38:47 +0800

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Nicholas Szeto on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
